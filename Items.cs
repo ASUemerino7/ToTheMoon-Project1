f813b22a792d580715c64a545d99a9b8dfeb1bab
Shailee Shah <43224434+sshah47@users.noreply.github.com>
2021-02-21 11:33:23 -0700

    Add files via upload

---
+++ Items.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToTheMoon-Project1
+{
+    public class Item
+    {
+        public int pantryid;
+        public int itemid;
+        public string itemname;
+        public int itemqty;
+        public string itemunit;
+        public string itemdesc;
+        public string itemcategory;
+    }
+}
---
+++ Pantries.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToTheMoon-Project1
+{
+    public class Pantry
+    {
+        public int pantryid;
+        public int userid;
+        public string pantryname;
+    }
+}
