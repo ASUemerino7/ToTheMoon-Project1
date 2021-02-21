d4d3a3c1b7a5788d7d94af7290be0d6ba71a8672
Shailee Shah <43224434+sshah47@users.noreply.github.com>
2021-02-21 11:36:02 -0700

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToTheMoon-Project1
+{
+    public class User
+    {
+        public int userid;
+        public string username;
+        public string password;
+        public string email;
+        public string city;
+        public string state;
+    }
+}
